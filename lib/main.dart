6d7c8cb4b0f15936169ca1bc9c8a396a18b1462c
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2023-10-21 01:32:10 +0100

    refactor: move app widget to main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'app.dart';
+import 'core/navigation/navigation_router.dart';
+import 'core/themes/custom_light_theme.dart';
 
 void main() {
   runApp(
@@ -9,4 +10,26 @@ void main() {
       child: App(),
     ),
   );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: (context, widget) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          child: widget!,
+        );
+      },
+      debugShowCheckedModeBanner: false,
+      title: 'Books app',
+      themeMode: ThemeMode.light,
+      theme: CustomLightTheme.getTheme(context),
+      initialRoute: initialRoute,
+      onGenerateRoute: NavigationRouter.generateRoute,
+    );
+  }
 }
