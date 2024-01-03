9cff5b5da779b5231ddfb2613f49bba1de719000
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2024-01-03 22:58:43 +0100

    refactor: remove hardcoded server uri

--- lib/core/providers/dio_provider.dart
+++ lib/core/providers/dio_provider.dart
@@ -2,10 +2,12 @@ import 'package:books_app_client/core/providers/jwt_state_provider.dart';
 import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../constants/env.dart';
+
 final dioProvider = Provider<Dio>((ref) {
   final jwt = ref.watch(jwtStateProvider);
   final options = BaseOptions(
-    baseUrl: 'http://192.168.1.2:5000/api/',
+    baseUrl: Env.baseUrl,
     connectTimeout: const Duration(seconds: 5),
     receiveTimeout: const Duration(seconds: 3),
     headers: {if (jwt.isNotEmpty) 'Authorization': 'Bearer $jwt'},
