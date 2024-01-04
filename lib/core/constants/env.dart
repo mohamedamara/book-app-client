40dc299f9c0684e5ecca4366280375c2029d08fe
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2024-01-04 19:53:21 +0100

    refactor: remove env field obfuscation

--- lib/core/constants/env.dart
+++ lib/core/constants/env.dart
@@ -2,8 +2,8 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(path: '.env')
-final class Env {
-  @EnviedField(varName: 'BASE_URL', obfuscate: true)
-  static final String baseUrl = _Env.baseUrl;
+@Envied()
+abstract class Env {
+  @EnviedField(varName: 'BASE_URL')
+  static const String baseUrl = _Env.baseUrl;
 }
